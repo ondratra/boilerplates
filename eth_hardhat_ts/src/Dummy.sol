c8fb30ff3df7581c78c584f3661497809a58ddde
ondratra <ondratra@gmail.com>
2023-01-31 22:14:57 +0100

    boilerplate update - Eth/Hardhat - package.json update and build minor fix

---
+++ eth_hardhat_ts/src/Dummy.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// this file and dummy contract need to exist for HardHat to succesfully merge the project.
+// It can be safely removed after this TypeChain issue is fixed and included in HardHat. 
+// https://github.com/dethcrypto/TypeChain/issues/772
+
+contract Dummy {
+    uint256 public dummy;
+}
