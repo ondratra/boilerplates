86dde64e28f16307f065e4032bb90a7a633acf78
ondratra <ondratra@gmail.com>
2023-02-28 23:28:39 +0100

    repo update - migration to yarn v3 II

--- boilerplates/002_qwik_web/vite.config.ts
+++ boilerplates/002_qwik_web/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikCity } from '@builder.io/qwik-city/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { qwikReact } from '@builder.io/qwik-react/vite'
 
 export default defineConfig(() => {
   return {
@@ -15,12 +16,18 @@ export default defineConfig(() => {
           outDir: 'dist/app/',
         },
       }),
+      qwikReact(),
       tsconfigPaths()
     ],
     preview: {
       headers: {
         'Cache-Control': 'public, max-age=600',
       },
+    },
+    server: {
+      host: process.env.SERVER_HOST || 'localhost',
+      port: process.env.SERVER_PORT ? parseInt(process.env.SERVER_PORT) : 5173,
+      open: process.env.VITE_OPEN_BROWSER_BY_DEFAULT ? !!parseInt(process.env.VITE_OPEN_BROWSER_BY_DEFAULT) : true,
     },
   };
 });
--- boilerplates/003_qwik_web3_ethers/vite.config.ts
+++ boilerplates/003_qwik_web3_ethers/vite.config.ts
@@ -24,5 +24,10 @@ export default defineConfig(() => {
         'Cache-Control': 'public, max-age=600',
       },
     },
+    server: {
+      host: process.env.SERVER_HOST || 'localhost',
+      port: process.env.SERVER_PORT ? parseInt(process.env.SERVER_PORT) : 5173,
+      open: process.env.VITE_OPEN_BROWSER_BY_DEFAULT ? !!parseInt(process.env.VITE_OPEN_BROWSER_BY_DEFAULT) : true,
+    },
   }
 })
